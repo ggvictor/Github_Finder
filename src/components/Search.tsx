0779f7a4519b3e9243be41291247457439bd6a29
ggvictor <victorrodriguessilva2@gmail.com>
2025-09-01 18:21:22 -0300

    atualizando o estilo

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,25 @@
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>
+};
+
+import { useState } from "react";
+
 import { BsSearch } from "react-icons/bs"
 
-const Search = () => {
+import classes from './Search.module.css'
+
+const Search = ({loadUser}: SearchProps) => {
+  const[userName, setUserName] = useState("");
+
   return (
-    <div>
+    <div className = {classes.search}>
         <h2>Busque por um usuário:</h2>
         <p>Conheça seus melhores repositórios</p>
-        <div>
-            <input type="text" placeholder='Digite o nome do usuário'/>
-            <button><BsSearch /></button>
+        <div className = {classes.search_container}>
+            <input type="text" placeholder='Digite o nome do usuário' onChange={(e) => setUserName(e.target.value)}/>
+            <button onClick = {() => loadUser(userName)}>
+              <BsSearch />
+            </button>
         </div>
     </div>
   )
