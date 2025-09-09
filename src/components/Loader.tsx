7fd53d0dfcbe0a6dca24c314f04ab6ce15bfc852
ggvictor <victorrodriguessilva2@gmail.com>
2025-09-09 15:17:34 -0300

    colocando loading

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import {FaSpinner} from 'react-icons/fa'
 
+import classes from './Loader.module.css'
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <>
+      <FaSpinner className={classes.loader}/>  
+    </>
   )
 }
 
